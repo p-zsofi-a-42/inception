8eb570fad78be75c428ca36b4f2eec0fd2894df5
Zsofi@InceptionVM <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-08-05 18:55:48 +0200

    fix: moving wp install to dockerfile

--- wordpress/setup.sh
+++ wordpress/setup.sh
@@ -1,25 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-# - php-fpm 		# PHP FastCGI Process Manager (PHP-FPM), which runs the PHP code
-# - php-mysql 		# php connection to db
-# - php-curl 		# http requests to other servers
-# - php-gd			# image manipulation
-# - php-mbstring 	# UTF-8 and multibyte chars
-# - php-xml 		# xml parsing
-# - php-intl 		# Internationalization (locales, formatting)
-# - php-zip			# handle zip archives
-apt install -y \
-	php-fpm \
-	php-mysql \
-	php-curl \
-	php-gd \
-	php-mbstring \
-	php-xml \
-	php-intl \
-	php-zip
+if [ ! -f /var/www/html/wp-config.php ]; then
 
-# Download latest wordpress version
-wget https://wordpress.org/latest.tar.gz
+    # create wp-config.php
+    # use DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
-# Extract 
-tar -xzvf latest.tar.gz
+fi
+
+#??
+exec php-fpm -F
