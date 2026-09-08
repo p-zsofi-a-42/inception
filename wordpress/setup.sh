5fa33adadab2c00d9cffd372e0073335d9ea6be6
Zsofi@InceptionVM <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-09-08 19:34:37 +0200

    wip: some wp script improvement

--- wordpress/setup.sh
+++ wordpress/setup.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 if [ ! -f /var/www/html/wp-config.php ]; then
+	cat > /var/www/html/wp-config.php <<EOF
+<?php
 
-    # create wp-config.php
-    # use DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
+define('DB_NAME',		'${DB_NAME}');
+define('DB_USER',		'${DB_USER}');
+define('DB_PASSWORD',	'${DB_PASSWORD}');
+/** MySQL hostname(:port if needed) */
+define('DB_HOST',		'${DB_HOST}');
+/** “The WordPress admin area and login must use HTTPS. (just a safeguard, in case in the future the port 80 will also be opened” */
+define('FORCE_SSL_ADMIN', true);
+?>
+EOF
 
 fi
 
 #: Starting the php-fpm service
-service php8.2-fpm start
-#??
-#exec php-fpm -F
+#service php8.2-fpm start
+exec php-fpm -F
