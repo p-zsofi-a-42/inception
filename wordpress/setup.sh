e16642ad2fe5ff284ca987c0ec83533eee6ed003
Zsofi@InceptionVM <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-09-09 17:46:15 +0200

    feat: wp works with correct entrypoint and script

--- wordpress/setup.sh
+++ wordpress/setup.sh
@@ -3,12 +3,13 @@
 if [ ! -f /var/www/html/wp-config.php ]; then
 	cat > /var/www/html/wp-config.php <<EOF
 <?php
+/** Telling wordpress how to connect to the db */
 
-define('DB_NAME',		'${DB_NAME}');
-define('DB_USER',		'${DB_USER}');
-define('DB_PASSWORD',	'${DB_PASSWORD}');
+define('WORDPRESS_DB_NAME',		'${DB_NAME}');
+define('WORDPRESS_DB_USER',		'${DB_USER}');
+define('WORDPRESS_DB_PASSWORD',	'${DB_PASSWORD}');
 /** MySQL hostname(:port if needed) */
-define('DB_HOST',		'${DB_HOST}');
+define('WORDPRESS_DB_HOST',		'${DB_HOST}');
 /** “The WordPress admin area and login must use HTTPS. (just a safeguard, in case in the future the port 80 will also be opened” */
 define('FORCE_SSL_ADMIN', true);
 ?>
@@ -16,6 +17,5 @@ EOF
 
 fi
 
-#: Starting the php-fpm service
-#service php8.2-fpm start
-exec php-fpm -F
+#: Starting the php-fpm service | -F keeping in foreground
+exec php-fpm8.2 -F
