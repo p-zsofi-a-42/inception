18982a0ecb17a58962216436acf024beb92392b3
p-zsofi-a-@-home <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-09-18 17:15:48 +0200

    fix: php config file

--- wordpress/setup.sh
+++ wordpress/setup.sh
@@ -10,15 +10,18 @@ define('DB_NAME',		'${WORDPRESS_DB_NAME}');
 define('DB_USER',		'${WORDPRESS_DB_USER}');
 define('DB_PASSWORD',	'${WORDPRESS_DB_PASSWORD}');
 /** MySQL hostname(:port if needed) */
-define('DB_HOST',		'${WORDPRESS_DB_HOST}');
-/** “The WordPress admin area and login must use HTTPS. (just a safeguard, in case in the future the port 80 will also be opened” */
-define('FORCE_SSL_ADMIN', true);
+define('DB_HOST',		'${WORDPRESS_DB_HOST}:${WORDPRESS_DB_PORT}');
+
+\$table_prefix = 'wp_'; // Only numbers, letters, and underscores please!
+
 /** Finding and loading wp-config.php. Load WordPress's main initialization/bootstrap*/
 require_once ABSPATH . 'wp-settings.php';
-?>
 EOF
 
 fi
+
+#NOTE: [php -l /var/www/html/wp-config.php] can check whether the PHP file is syntactically valid
+
 
 #: Starting the php-fpm service | -F keeping in foreground
 exec php-fpm8.2 -F 
